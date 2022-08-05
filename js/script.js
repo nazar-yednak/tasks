eaa4344addf0d711798877175364717d1aa2b836
nazar-yednak <93836409+nazar-yednak@users.noreply.github.com>
2022-08-05 18:20:40 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const elementInput = document.querySelectorAll('.num')
+console.log(elementInput);
+const btn = document.querySelector('#task')
+console.log(btn);
+const result = document.querySelector('#result')
+console.log(result);
+
+  let totalValue = 0;
+btn.addEventListener("input", (event)=> event.curentTarget.value + totalValue;)
+
+ console.log( event.curentTarget.value);
+
+// const array = [1, 2, 3, 4]
+// const  valueTotal = 
+// array.reduce((acc,element) => acc + element, 0   )
+
+
+// console.log(valueTotal);
